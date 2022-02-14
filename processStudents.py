6ed7d468b2919186ab91166118e265969db0be10
Nichol Vacca <nichol_vacca1@baylor.edu>
2022-02-14 15:45:30 -0600

    quiz 1

--- processStudents.py
+++ processStudents.py
@@ -22,54 +22,65 @@ through the logic of the problem.
 
 import csv
 
-
 # create a file object to open the file in read mode
 
-
+infile = open('students.csv', 'r')
 
 # create a csv object from the file object
 
+student_file = csv.reader(infile, delimiter = ',')
 
 #skip the header row
 
+next(student_file)
 
 #create an outfile object for the pocessed record
 
-
+outfile = open('processedStudents.csv', 'w')
 
 #create a new dictionary named 'student_dict'
 
-
+student_dict = dict()
 
 #use a loop to iterate through each row of the file
 
     #check if the GPA is below 3.0. If so, write the record to the outfile
     
-        
-
-
+for student in student_file:
+    if float(student[8]) < float(3.0):
+        outfile.write(student[0] + ',' + student[1] + ',' + student[2] + ',' +
+        student[3] + ',' + student[4] + ',' + student[5] + ',' +
+        student[6] + ',' + student[7] + ',' + student[8] + '\n'
+        )
+            
 
     # append the record to the dictionary with the student id as the Key
     # and the value as the GPA
     
-
-
+        student_dict[student[0]]= student[8]
 
 
 
 #print the entire dictionary
 
+print(student_dict)
 
 #Print the student id 
+student_id_choice = '567890123'
 
+if student_id_choice in student_dict:
+    print(student_id_choice)
 
 #print out the corresponding GPA from the dictionary
 
+    print(student_dict['567890123'])
 
-
+else: 
+    print('student is not in the list')
+ 
 #close the outfile
 
-
+outfile.close()
 
 
 
